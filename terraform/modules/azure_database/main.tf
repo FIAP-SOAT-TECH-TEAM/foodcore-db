de2c58fb1e463fea23ffc006f3e05adcc797d629
Pedro Ferrarezzo <ferrarezzolabs@outlook.com>
2025-09-02 22:30:33 -0300

    fix: set public_network_access_enabled to false

--- terraform/modules/azure_database/main.tf
+++ terraform/modules/azure_database/main.tf
@@ -1,3 +1,6 @@
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/postgresql_flexible_server
+# https://learn.microsoft.com/en-us/azure/developer/terraform/deploy-postgresql-flexible-server-database?tabs=azure-cli
+
 resource "azurerm_network_security_group" "db_nsg" {
   name                = "${var.dns_prefix}-db-nsg"
   location            = var.location
@@ -22,16 +25,17 @@ resource "azurerm_subnet_network_security_group_association" "db_assoc" {
 }
 
 resource "azurerm_postgresql_flexible_server" "psqlflexibleserver" {
-  name                   = "${var.dns_prefix}-psqlflexibleserver"
-  resource_group_name    = var.resource_group_name
-  location               = var.location
-  version                = var.db_version
-  administrator_login    = var.administrator_login
-  administrator_password = var.administrator_password
-  storage_mb             = var.db_storage_mb
-  sku_name               = var.db_sku_name
-  delegated_subnet_id    = var.db_subnet_id
-  private_dns_zone_id    = var.private_dns_zone_id
+  name                          = "${var.dns_prefix}-psqlflexibleserver"
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  version                       = var.db_version
+  administrator_login           = var.administrator_login
+  administrator_password        = var.administrator_password
+  storage_mb                    = var.db_storage_mb
+  sku_name                      = var.db_sku_name
+  delegated_subnet_id           = var.db_subnet_id
+  private_dns_zone_id           = var.private_dns_zone_id
+  public_network_access_enabled = false
 }
 
 resource "azurerm_postgresql_flexible_server_database" "foodcore_pgsql_database" {
